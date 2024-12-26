15b3c74e726853555a42205fb775a3c120c2f379
albyscotney <alby.scotney@icloud.com>
2024-12-26 12:40:33 +0000

    simplify: remove navigator object

--- src/architecture/user.py
+++ src/architecture/user.py
@@ -2,11 +2,14 @@
 A class to store the attributes of a user and their corresponding data
 """
 
+import subprocess
 from dataclasses import dataclass, field
 
 import pandas as pd
 
 from src.architecture.banks.banks import Bank
+from src.architecture.secrets.secret_handling import SecretManager
+from src.utils.api import API
 
 
 @dataclass
@@ -16,10 +19,13 @@ class User:
     """
 
     username: str
+    secret_loc: str
+    secret: SecretManager = field(repr=False, init=False)
     banks: dict = field(repr=False, init=False, default_factory=dict)
     transactions: pd.DataFrame = field(repr=False, init=False)
 
     def __post_init__(self):
+        self.secret = SecretManager(self.secret_loc)
         self.banks = {}
         self.transactions = pd.DataFrame(
             columns=["date", "amount", "merchant", "bank", "account", "merchant_id"]
@@ -40,3 +46,20 @@ class User:
             self.transactions = pd.concat(
                 [self.transactions, bank.retrieve_account_data()]
             )
+
+    def get_banks_list(self, save_target: str) -> None:
+        """
+        Retrieve a list of banks that can be connected to.
+        """
+        API.get_banks_list(self.secret.access_token["access"], save_target)
+
+    def shell_function(self, command: str) -> None:
+        """
+        Run a shell command inputted as a string.
+        """
+        result = subprocess.run(
+            command, shell=True, capture_output=True, text=True, check=True
+        )
+        print(result.stdout)
+        if result.stderr:
+            print(result.stderr)
